24e40458ebafdaa67d227c59ee5154b5833cac63
sofishaa <22fmif.s.hunkalo@std.npu.edu.ua>
2023-09-05 18:15:09 +0300

    HomeWork9

---
+++ HW9/HW9(a)/Shape.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW9
+{
+    abstract class Shape
+    {
+
+        private string name;
+        public string Name { get { return name; } }
+
+        public Shape(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract double Area();
+        public abstract double Perimeter();
+
+        public abstract void Input();
+        public abstract void Output();
+
+    }
+}
